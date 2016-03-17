36c33fa2794e263174d975c8ff59f69962ae7c67
shadabgaur <shadab.gaur@gmail.com>
2016-03-17 12:50:50 +1100

    first commit

---
+++ build/gwt/gen/com/google/gwt/lang/com_00046javacodegeeks_00046gradle_00046gwt_00046integration_00046build_00046GradleGWTIntegrationExampleJCG__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046javacodegeeks_00046gradle_00046gwt_00046integration_00046build_00046GradleGWTIntegrationExampleJCG__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
